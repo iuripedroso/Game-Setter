392b65e7c4e97d6d1fd88b459aa72ca1d821c75f
iuripedroso <iuripedroso11072005@gmail.com>
2025-12-01 22:15:53 -0300

    rotas atualizadas

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import MainPage from "./mainpage";
 import LoginPage from "./loginPage";
+import ProfilePage from "./ProfilePage";
 
 function App() {
     const [logado, setLogado] = useState(false);
+    const [page, setPage] = useState("main"); // main | profile
 
-    return (
-        <>
-            {!logado ? (
-                <LoginPage onLogin={() => setLogado(true)} />
-            ) : (
-                <MainPage />
-            )}
-        </>
-    );
+    if (!logado) {
+        return <LoginPage onLogin={() => setLogado(true)} />;
+    }
+
+    if (page === "profile") {
+        return <ProfilePage goToMain={() => setPage("main")} />;
+    }
+
+    return <MainPage goToProfile={() => setPage("profile")} />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
